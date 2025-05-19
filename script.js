5d789d08c631e33a539f7a66706badf3ae9d8b30
yuta082221 <homan_yuta@cellmuller.com>
2025-05-19 12:49:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,18 @@ document.addEventListener("DOMContentLoaded", () => {
   }, {
     threshold: 0.1
   });
-
+// ボタンがクリックされたときに実行する関数 (例)
+function trackButtonClick(buttonId, buttonText) {
+  dataLayer.push({
+    'event': 'custom_button_click', // GTMでトリガーとして使用するイベント名
+    'buttonId': buttonId,           // ボタンのIDなどの識別子
+    'buttonText': buttonText,       // ボタンのテキストなど
+    // 必要に応じて他の情報も追加できます
+    // 'productId': 'SKU123',
+    // 'buttonLocation': 'header',
+  });
+}
+  
 elements.forEach(el => {
     el.style.opacity = 0;
     el.style.transform = "translateY(30px)";
